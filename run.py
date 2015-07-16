4c2760704440f0f9b2e88d7152c5c2c5607446b6
Rostyslav Bevz <rostyslav.bevz@lge.com>
2015-07-16 13:52:10 +0300

    Small updates

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from convertcelcius.Convert import celsius2fahren
 
 user_val = input("Please, enter temperature in Celcius:")
--- test/testConvert.py
+++ test/testConvert.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import unittest
 from convertcelcius.Convert import celsius2fahren
 
